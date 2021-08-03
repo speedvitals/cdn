7a1d6a6d47a40b6878265cf896b7a66a9138edd1
Kashish Kumawat <kashishkumawat@gmail.com>
2021-08-03 15:35:22 +0530

    Update hotjar.js

--- hotjar.js
+++ hotjar.js
@@ -1,4 +1,3 @@
-<script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:2532435,hjsv:6};
@@ -7,4 +6,3 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
